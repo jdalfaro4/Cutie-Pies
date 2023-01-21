7c39c8728d56efbf121c9eda74eb542b35b5168f
Cj adorna <cjadorna@hotmail.com>
2023-01-21 14:42:16 -0600

    removed foundation// removed containers for ease of development and testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-$(document).foundation()
+$(document).ready(function() {
+    $('#atcBtn').click(function() {
+        console.log("add to cart button pressed")
+        var flavor = $(this).data('flavor');
+        localStorage.setItem("AddedToCart", flavor)
+    });
+})
