6c140aad3a0abfbe1b0a171026e6aaf7892fea0f
Cj adorna <cjadorna@hotmail.com>
2023-01-21 15:20:23 -0600

    rmBtn working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,17 @@ $(document).ready(function() {
         localStorage.setItem("AddedToCart", JSON.stringify(items));
         $(this).addClass('hide');
         $(this).next('.rmBtn').removeClass('hide');
+        console.log(items)
     });
-})
+
+    $('.rmBtn').click(function(){
+        console.log("remove from cart button pressed")
+        var flavor = $(this).data('flavor')
+        var index = items.indexOf(flavor);
+        items.splice(index, 1);
+        localStorage.setItem("AddedToCart", JSON.stringify(items));
+        $(this).addClass('hide');
+        $(this).prev('.atcBtn').removeClass('hide');
+
+    });
+});
