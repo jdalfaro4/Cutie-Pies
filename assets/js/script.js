f88717ad548d80d5d8d3c76b661bea541925e396
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-01-30 20:31:47 -0600

    worked on API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var itemsWithPrices = [];
 
 
 $(document).ready(function() {
-    
+
     $('.sidenav').sidenav();
-    
-    //ADD TO CART button 
+
+    //ADD TO CART button
     $('.atcBtn').click(function() {
         var flavor = $(this).data('flavor');
         var price = $(this).prev('#price').text();
@@ -24,7 +24,7 @@ $(document).ready(function() {
         var flavor = $(this).data('flavor')
         var price = $(this).prev().prev('#price').text();
         var flavorPrice = flavor + "-" + price;
-        var index = items.indexOf(flavorPrice); 
+        var index = items.indexOf(flavorPrice);
         items.splice(index, 1);
         localStorage.setItem("AddedToCart", JSON.stringify(items));
         console.log(items)
@@ -44,14 +44,13 @@ $(document).ready(function() {
             var price = currentCart[i].split('-')[1];
             cartSticky.innerHTML += "<li>" + flavor + "</li>";
             cartStickyPrices.innerHTML += "<li>" + price + "</li>";
-        
+
         }
         cartStickyPrices.innerHTML += "</ul>";
-        cartSticky.innerHTML += "</ul>";  
+        cartSticky.innerHTML += "</ul>";
         console.log('Cart sticky updated')
     }
 });
-
 
 var apiurl2 ="https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=fd0d519865b662dfd044fa27b1e7cbf9&photoset_id=72177720305535430&user_id=197587105%40N08&format=json&nojsoncallback=1";
 $(document).ready(function(){
@@ -66,4 +65,55 @@ $(document).ready(function(){
         })
     })
 });
+// const checkoutButton = document.querySelector('#checkoutButton');
 
+
+function checkoutButton() {
+    const apiurl3 = "https://api.giphy.com/v1/gifs/cKVUvW2RMJ6UaBzJxn?api_key=8rPbTKt8rKzlOnJfCxcQN8gPIo4aqgDE";
+
+    fetch(apiurl3)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function cartCelebration
+// var apiurl3  ="https://api.giphy.com/v1/gifs/cKVUvW2RMJ6UaBzJxn?api_key=8rPbTKt8rKzlOnJfCxcQN8gPIo4aqgDE";
+
+
+// $(document).ready(function(){
+//     $.getJSON(apiurl3,function(response){
+//          console.log(response);
+//          $.each(response.photoset.photo,function(idx,photo){
+//             console.log(photo);
+//             //https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+//             // var imageUrl ="https://live.staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
+//             console.log(imageUrl);
+//             $("#"+photo.id).attr ("src",imageUrl);
+//         })
+//     })
+// });
